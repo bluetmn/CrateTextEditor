13eefd8b65bb62ca452ee1eaf6aab5cb60c249c1
Thomas Ryan <thomasryan2118@gmail.com>
2025-01-23 21:07:37 -0600

    print centered welcome message

--- src/crate.c
+++ src/crate.c
@@ -11,6 +11,8 @@
 
 /*** DEFINES ***/
 
+#define CRATE_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** DATA ***/
@@ -161,9 +163,30 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows ; y++) {
-        abAppend(ab, "~", 3);
+        if (y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), 
+            "Crate editor == version %s", CRATE_VERSION);
+
+            // ensure message does not wrap - ugly
+            if (welcomelen > E.screencols) {
+                welcomelen = E.screencols;
+            }
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--) {
+                abAppend(ab, " ", 1);
+            }
+            abAppend(ab, welcome, welcomelen);
+        }
+        else {
+            abAppend(ab, "~", 1);
+        }
         // <esc>[K to clear right of cursor
-        aAppend(ab, "\x1b[K", 3);
+        abAppend(ab, "\x1b[K", 3);
 
         if (y != E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
