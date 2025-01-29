d442ef501bc3fa46870ced62cc0275a7e596cc4d
Thomas Ryan <thomasryan2118@gmail.com>
2025-01-29 17:09:41 -0600

    save as implementation

--- src/crate.c
+++ src/crate.c
@@ -68,6 +68,8 @@ struct editorConfig E;
 /*** ---------- PROTOTYPES ---------- ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** ---------- TERMINAL ---------- ***/
 
@@ -449,7 +451,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        return;
+        E.filename = editorPrompt("Save as: %s");
     }
 
     int len;
@@ -770,6 +772,35 @@ void editorProcessKeypress() {
             break;
     }
     quit_times = CRATE_QUIT_TIMES;
+}
+
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+        }
+        else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
 }
 
 /*** ---------- INIT ---------- ***/
