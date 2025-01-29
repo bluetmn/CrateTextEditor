b8d0e5286b25aab5ff76724164d40a09a3312288
Thomas Ryan <thomasryan2118@gmail.com>
2025-01-29 16:23:47 -0600

    implement enter key

--- src/crate.c
+++ src/crate.c
@@ -283,22 +283,25 @@ void editorUpdateRow(erow *row) {
     row->rsize = idx;
 }
 
-void editorAppendRow(char *s, size_t len) {
-        // allocate enough room for another erow
-        E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len) {
+    if (at < 0 || at > E.numrows) {
+        return;
+    }
 
-        int at = E.numrows;
-        E.row[at].size = len;
-        E.row[at].chars = malloc(len + 1);
-        memcpy(E.row[at].chars, s, len);
-        E.row[at].chars[len] = '\0';
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
-        E.row[at].rsize = 0;
-        E.row[at].render = NULL;
-        editorUpdateRow(&E.row[at]);
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
 
-        E.numrows++;
-        E.dirty++;
+    E.row[at].rsize = 0;
+    E.row[at].render = NULL;
+    editorUpdateRow(&E.row[at]);
+
+    E.numrows++;
+    E.dirty++;
 }
 
 
@@ -352,10 +355,26 @@ void editorRowDelChar(erow *row, int at) {
 
 void editorInsertChar(int c) {
     if (E.cy == E.numrows) {
-        editorAppendRow("", 0);
+        editorInsertRow(E.numrows, "", 0);
     }
     editorRowInsertChar(&E.row[E.cy], E.cx, c);
     E.cx++;
+}
+
+void editorInsertNewline() {
+    if (E.cx == 0) {
+        editorInsertRow(E.cy, "", 0);
+    }
+    else {
+        erow *row = &E.row[E.cy];
+        editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+        row = &E.row[E.cy];
+        row->size = E.cx;
+        row->chars[row->size] = '\0';
+        editorUpdateRow(row);
+    }
+    E.cy++;
+    E.cx = 0;
 }
 
 void editorDelChar() {
@@ -420,7 +439,7 @@ void editorOpen(char *filename) {
                 linelen--;
             }
             
-            editorAppendRow(line, linelen);
+            editorInsertRow(E.numrows, line, linelen);
         }
     }
     free(line);
@@ -674,6 +693,7 @@ void editorProcessKeypress() {
 
     switch (c) {
         case '\r':
+            editorInsertNewline();
             break;
         
         case CTRL_KEY('q'):
